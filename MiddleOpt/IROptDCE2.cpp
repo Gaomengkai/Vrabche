560f753fe3b8af57e700a9a4973427ccf59d0ad7
Merky <gaomengkai0@outlook.com>
2023-08-07 21:36:53 +0800

    remove r log

--- MiddleOpt/IROptDCE2.cpp
+++ MiddleOpt/IROptDCE2.cpp
@@ -139,18 +139,19 @@ void IROptDCE2::run()
                 if (isUseless[inst]) {
                     inst->setDeleted(true);
                     hasChanged = true;
-                    std::cout << "del ";
-                    if (auto store = dynamic_pointer_cast<StoreInst>(inst)) {
-                        std::cout << "store " << store->getFrom()->getName() << "->"
-                                  << store->getTo()->getName() << std::endl;
-                    } else
-                        std::cout << inst->getName() << std::endl;
+                    //                    std::cout << "del ";
+                    //                    if (auto store = dynamic_pointer_cast<StoreInst>(inst)) {
+                    //                        std::cout << "store " << store->getFrom()->getName()
+                    //                        << "->"
+                    //                                  << store->getTo()->getName() << std::endl;
+                    //                    } else
+                    //                        std::cout << inst->getName() << std::endl;
                 }
             }
             // 真删除
             bb->_instructions.remove_if([](const SPInst& inst) { return inst->isDeleted(); });
             // 结果：
-            std::cout << bb->_instructions.size() << std::endl;
+            //            std::cout << bb->_instructions.size() << std::endl;
         }
 
 
